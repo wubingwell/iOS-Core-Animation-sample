d5dba6bb4050d333254ad83da6123fe9d0a5e0cf
wub <wub@xuele.net>
2016-01-12 19:12:48 +0800

    new project

---
+++ 007隐式动画/ImplicitAnimation/ImplicitAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImplicitAnimation
+//
+//  Created by bing on 16/1/12.
+//  Copyright © 2016年 bing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 007隐式动画/ImplicitAnimation/ImplicitAnimationTests/ImplicitAnimationTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImplicitAnimationTests.swift
+//  ImplicitAnimationTests
+//
+//  Created by bing on 16/1/12.
+//  Copyright © 2016年 bing. All rights reserved.
+//
+
+import XCTest
+@testable import ImplicitAnimation
+
+class ImplicitAnimationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
